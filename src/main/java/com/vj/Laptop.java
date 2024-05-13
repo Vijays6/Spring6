5b5a20c74bd469c8a6edc89f816ef2e84dc7a2f4
Vijay <vijayvj.cjs@gmail.com>
2024-05-13 21:45:10 +0530

    Added Constructor Injection

--- src/main/java/com/vj/Laptop.java
+++ src/main/java/com/vj/Laptop.java
@@ -2,6 +2,8 @@ package com.vj;
 
 public class Laptop {
     private String brand;
+    private Ssd ssd;
+
 
     public Ssd getSsd() {
         return ssd;
@@ -11,10 +13,13 @@ public class Laptop {
         this.ssd = ssd;
     }
 
-    private Ssd ssd;
-
     public Laptop() {
         System.out.println("Obj Created");
+    }
+
+    public Laptop(String warranty) {
+        System.out.println("Arg Constructor");
+        System.out.println(warranty);
     }
 
     public String getBrand() {
